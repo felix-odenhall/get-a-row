93f0c1bc5dd7a93698f0a902c5b7cdd208ca2398
Felix Odenhall <felix.odenhall@futurice.com>
2022-12-14 09:15:47 +0100

    add function to change name

--- src/components/StartGameComponent.tsx
+++ src/components/StartGameComponent.tsx
@@ -1,11 +1,18 @@
 interface StartGameComponentProps {
   activePlayer: string;
+  username: string;
+  setUsername: (username: string) => void;
+  setActivePlayer: (activePlayer: string) => void;
 }
 
-function StartGameComponent({ activePlayer }: StartGameComponentProps) {
+function StartGameComponent({
+  activePlayer,
+  setUsername,
+}: StartGameComponentProps) {
   return (
     <>
       <h2>Welcome {activePlayer}</h2>
+      <button onClick={() => setUsername("")}>Change name</button>
       <button>Let's Play</button>
     </>
   );
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,20 +1,44 @@
-import React, { useState } from "react";
+import { useEffect, useState } from "react";
 import CreateUserForm from "../components/CreateUserForm";
 import StartGameComponent from "../components/StartGameComponent";
 
 function Home() {
   const [username, setUsername] = useState<string>("");
   const [activePlayer, setActivePlayer] = useState<string>("");
-  return (
-    <>
-      <CreateUserForm
-        username={username}
-        setUsername={setUsername}
-        setActivePlayer={setActivePlayer}
-      />
-      <StartGameComponent activePlayer={activePlayer} />
-    </>
-  );
+
+  const hasAUsernameFn = () => {
+    if (activePlayer === "") {
+      return (
+        <>
+          <CreateUserForm
+            username={username}
+            setUsername={setUsername}
+            setActivePlayer={setActivePlayer}
+          />
+        </>
+      );
+    } else {
+      return (
+        <>
+          <StartGameComponent
+            setActivePlayer={setActivePlayer}
+            activePlayer={activePlayer}
+            username={username}
+            setUsername={setUsername}
+          />
+        </>
+      );
+    }
+  };
+
+  useEffect(() => {
+    console.log("change happened");
+    setActivePlayer("");
+  }, [username]);
+
+  const hasAUser = hasAUsernameFn();
+
+  return <>{hasAUser}</>;
 }
 
 export default Home;
