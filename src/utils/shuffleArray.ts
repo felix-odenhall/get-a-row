3f98b88571115c59a7b0015eb92f6f4d71e50bd4
Felix Odenhall <felix.odenhall@gmail.com>
2023-02-06 10:50:17 +0100

    move shuffle to utils

---
+++ src/utils/shuffleArray.ts
@@ -0,0 +1,10 @@
+import { BingoData } from "../store/zustandStore";
+
+export const shuffleArray = ([...arr]): BingoData[] => {
+  let m = arr.length;
+  while (m) {
+    const i = Math.floor(Math.random() * m--);
+    [arr[m], arr[i]] = [arr[i], arr[m]];
+  }
+  return arr;
+};
