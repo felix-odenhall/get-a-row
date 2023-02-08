0f7f022c169614c6bc43f8b14f124a6fb2767aca
Felix Odenhall <felix.odenhall@gmail.com>
2023-02-08 09:26:50 +0100

    updating style, remove unused imports

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,20 +1,22 @@
 import {
   Box,
   Button,
-  Container,
+  CloseButton,
   Drawer,
   DrawerBody,
-  DrawerCloseButton,
   DrawerContent,
   DrawerFooter,
   DrawerHeader,
   DrawerOverlay,
-  Input,
+  List,
+  ListItem,
   Text,
+  UnorderedList,
   useDisclosure,
 } from "@chakra-ui/react";
 import useStore from "../../store/zustandStore";
 import HamburgerIcon from "./HamburgerIcon";
+import NavbarListButton from "./NavbarListButton";
 
 export const Header = () => {
   const {
@@ -39,68 +41,79 @@ export const Header = () => {
 
   return (
     <>
-      <Container
+      <Box
+        as="header"
         pos="sticky"
         top="0"
+        left="0"
+        h="8vh"
         w="100%"
-        h="10vh"
+        px="2"
         bg="rgba(255,255,255,0.95)"
         display="flex"
         alignItems="center"
         justifyContent="flex-end"
-        boxShadow="base"
         zIndex={5}
+        shadow={isOpen ? "base" : "none"}
       >
-        <HamburgerIcon onClick={onOpen} isOpen={isOpen} />
-        {/* <Button colorScheme="teal" onClick={onOpen}>
-          Menu
-        </Button> */}
-        <Drawer isOpen={isOpen} placement="right" onClose={onClose}>
+        {!isOpen ? (
+          <HamburgerIcon onClick={onOpen} />
+        ) : (
+          <CloseButton fontSize="xl" mr="2" />
+        )}
+        <Drawer isOpen={isOpen} placement="right" onClose={onClose} size="full">
           <DrawerOverlay
-            mt="10vh"
+            mt="8vh"
             bg="rgba(255,255,255,0.25)"
             backdropFilter="auto"
             backdropBlur="6px"
           />
-          <DrawerContent h="90vh" mt="10vh" bg="red">
-            <DrawerHeader>Create your account</DrawerHeader>
+          <DrawerContent h="fill" w="30%" mt="8.5vh" bg="white">
+            <DrawerHeader
+              w="100%"
+              textAlign="center"
+              mb="2"
+              borderBottom="1px"
+              fontSize="3xl"
+            >
+              Menu
+            </DrawerHeader>
 
-            <DrawerBody w="90%">
-              <Button
-                colorScheme="orange"
-                size="md"
-                bgGradient="linear(to-b, orange.400, tomato)"
-                fontWeight="medium"
-                fontSize="lg"
-                onClick={pickNewTasks}
-                boxShadow="base"
-                mb="8"
-              >
-                Pick New Tasks
-              </Button>
+            <DrawerBody px="0">
+              <UnorderedList listStyleType="none" m="0">
+                <NavbarListButton
+                  onClick={pickNewTasks}
+                  buttonName="Reset game"
+                />
+                <NavbarListButton
+                  onClick={pickNewTasks}
+                  buttonName="Pick new tasks"
+                />
+                <NavbarListButton
+                  onClick={pickNewTasks}
+                  buttonName="Change name"
+                />
+              </UnorderedList>
             </DrawerBody>
 
-            <DrawerFooter>
-              <Button variant="outline" mr={3} onClick={onClose}>
-                Cancel
-              </Button>
-              <Button colorScheme="blue">Save</Button>
-            </DrawerFooter>
+            <DrawerFooter></DrawerFooter>
           </DrawerContent>
         </Drawer>
-      </Container>
+      </Box>
 
       <Box
         pos="sticky"
-        top="10vh"
+        top="8vh"
         zIndex={3}
         h="8"
-        bg="white"
+        bg="rgba(255,255,255,0.95)"
         display="flex"
         justifyContent="flex-end"
+        alignItems="center"
+        boxShadow="base"
       >
         {pickedTasks.length !== 0 && pickedTasks.length <= 9 && (
-          <Text>{`${pickedTasks.length} / 9 Tasks`}</Text>
+          <Text mr="3">{`Selected: ${pickedTasks.length} / 9`}</Text>
         )}
       </Box>
     </>
