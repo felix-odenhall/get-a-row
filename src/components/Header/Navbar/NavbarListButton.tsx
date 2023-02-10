fd4a6c41ed5952722c83dfc5fb4250d13946adf6
Felix Odenhall <felix.odenhall@gmail.com>
2023-02-10 12:15:22 +0100

    update button style

--- src/components/Header/Navbar/NavbarListButton.tsx
+++ src/components/Header/Navbar/NavbarListButton.tsx
@@ -1,4 +1,4 @@
-import { Button, ListItem } from "@chakra-ui/react";
+import { Box, ListItem, Text } from "@chakra-ui/react";
 
 interface NavbarListButtonProps {
   onClick: () => void;
@@ -8,21 +8,20 @@ interface NavbarListButtonProps {
 const NavbarListButton = ({ onClick, buttonName }: NavbarListButtonProps) => {
   return (
     <ListItem>
-      <Button
+      <Box
+        as="button"
         bg="transparent"
         w="100%"
         h="20"
-        size="md"
         fontWeight="bold"
         fontSize="xl"
         onClick={onClick}
         borderBottom="1px"
         borderColor="gray.200"
-        borderRadius="none"
         mb="8"
       >
-        {buttonName}
-      </Button>
+        <Text>{buttonName}</Text>
+      </Box>
     </ListItem>
   );
 };
