f3b8a4e5a05ce178feb40d5ea7c900e17ce6805b
Felix Odenhall <felix.odenhall@gmail.com>
2023-02-10 14:57:05 +0100

    adding test for adding username

--- cypress/e2e/test.cy.ts
+++ cypress/e2e/test.cy.ts
@@ -1,7 +1,19 @@
 describe("The Home Page", () => {
-  it("successfully loads", () => {
+  beforeEach(() => {
     cy.visit("http://localhost:3000");
+  });
+
+  it("successfully loads", () => {
     cy.get("[data-cy=user-input]").should("be.visible");
     cy.get("[data-cy=submit-button]").should("be.visible");
+  });
+  it("Should only move on when a username is added", () => {
+    cy.get("[data-cy=submit-button]").click();
+    cy.get("[data-cy=user-input]").should("exist");
+    cy.get("[data-cy=submit-button]").should("exist");
+    cy.get("[data-cy=user-input]").type("MyUsername1");
+    cy.get("[data-cy=submit-button]").click();
+    cy.get("[data-cy=user-input]").should("not.exist");
+    cy.get("[data-cy=submit-button]").should("not.exist");
   });
 });
