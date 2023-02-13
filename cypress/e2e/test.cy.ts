e6f030b892e32aa0ecf97a22bd8767863cbcdae8
Felix Odenhall <felix.odenhall@gmail.com>
2023-02-13 12:33:47 +0100

    adding test for pick tasks and start game

--- cypress/e2e/test.cy.ts
+++ cypress/e2e/test.cy.ts
@@ -37,8 +37,9 @@ describe("Pick tasks", () => {
     cy.get("[data-cy=userInput]").type("MyUsername1");
     cy.get("[data-cy=submitButton]").click();
   });
-  it("Should be able to pick tasks when a username is added", () => {
+  it("Should be able to select and unselect tasks when a username is added", () => {
     cy.get("[data-cy=pickTasksContainer]").should("be.visible");
+    cy.get("[data-cy=pickTaskList]>li").should("have.length.gt", 9);
     cy.get("[data-cy=pickTasksContainer]>p").contains("Pick 9 tasks");
     cy.get("[data-cy=pickTaskList]>li").eq(2).click();
     cy.get("[data-cy=pickTasksContainer]>p").contains("Pick 8 more tasks");
@@ -50,10 +51,38 @@ describe("Pick tasks", () => {
     cy.get("[data-cy=pickTaskList]>li").eq(9).click();
     cy.get("[data-cy=pickTaskList]>li").eq(2).click();
     cy.get("[data-cy=pickTasksContainer]>p").contains("Pick 4 more tasks");
+    cy.get("[data-cy=pickTaskList]>li").eq(2).click();
+    cy.get("[data-cy=pickTaskList]>li").eq(12).click();
+    cy.get("[data-cy=pickTaskList]>li").eq(1).click();
+    cy.get("[data-cy=pickTasksContainer]>p").contains("Pick 7 more tasks");
+  });
+});
+
+describe("Start game", () => {
+  const username = "Username";
+  before(() => {
+    cy.visit("http://localhost:3000");
+    cy.get("[data-cy=userInput]").type(`${username}`);
+    cy.get("[data-cy=submitButton]").click();
+    cy.get("[data-cy=pickTaskList]>li").eq(0).click();
+    cy.get("[data-cy=pickTaskList]>li").eq(1).click();
+    cy.get("[data-cy=pickTaskList]>li").eq(2).click();
     cy.get("[data-cy=pickTaskList]>li").eq(3).click();
     cy.get("[data-cy=pickTaskList]>li").eq(4).click();
+    cy.get("[data-cy=pickTaskList]>li").eq(5).click();
+    cy.get("[data-cy=pickTaskList]>li").eq(6).click();
     cy.get("[data-cy=pickTaskList]>li").eq(7).click();
     cy.get("[data-cy=pickTaskList]>li").eq(8).click();
-    cy.get("[data-cy=pickTasksContainer]>button").contains("Start game");
+  });
+  it("Should be able to start game when 9 tasks are picked", () => {
+    cy.get("[data-cy=pickTasksContainer]>p").should("not.exist");
+    cy.get("[data-cy=startGameButton]").should("be.visible");
+    cy.get("[data-cy=startGameButton]").click();
+    cy.get("[data-cy=pickTasksContainer]").should("not.exist");
+    cy.get("[data-cy=usersGameBoardLabel]").should("be.visible");
+    cy.get("[data-cy=usersGameBoardLabel]").contains(`${username}`);
+    cy.get("[data-cy=bingoBoard]").should("be.visible");
+    cy.get("[data-cy=bingoTile]").should("be.visible");
+    cy.get("[data-cy=bingoTile]").should("have.length", 9);
   });
 });
