a0408d4a6eef63e684c2a02fec62b9ef8d4acf25
Felix Odenhall <felix.odenhall@gmail.com>
2023-02-14 16:15:35 +0100

    add e2e test for selecting tasks

---
+++ cypress/e2e/selectTasks.cy.ts
@@ -0,0 +1,26 @@
+describe("Select tasks", () => {
+  before(() => {
+    cy.visit("http://localhost:3000");
+    cy.get("[data-cy=userInput]").type("user123");
+    cy.get("[data-cy=submitButton]").click();
+  });
+  it("should be able to select and unselect tasks", () => {
+    cy.get("[data-cy=pickTasksContainer]").should("be.visible");
+    cy.get("[data-cy=pickTaskList]>li").should("have.length.gt", 9);
+    cy.get("[data-cy=pickTasksContainer]>p").contains("Pick 9 tasks");
+    cy.get("[data-cy=pickTaskList]>li").eq(2).click();
+    cy.get("[data-cy=pickTasksContainer]>p").contains("Pick 8 more tasks");
+    cy.get("[data-cy=pickTaskList]>li").eq(2).click();
+    cy.get("[data-cy=pickTasksContainer]>p").contains("Pick 9 tasks");
+    cy.get("[data-cy=pickTaskList]>li").eq(0).click();
+    cy.get("[data-cy=pickTaskList]>li").eq(1).click();
+    cy.get("[data-cy=pickTaskList]>li").eq(12).click();
+    cy.get("[data-cy=pickTaskList]>li").eq(9).click();
+    cy.get("[data-cy=pickTaskList]>li").eq(2).click();
+    cy.get("[data-cy=pickTasksContainer]>p").contains("Pick 4 more tasks");
+    cy.get("[data-cy=pickTaskList]>li").eq(2).click();
+    cy.get("[data-cy=pickTaskList]>li").eq(12).click();
+    cy.get("[data-cy=pickTaskList]>li").eq(1).click();
+    cy.get("[data-cy=pickTasksContainer]>p").contains("Pick 7 more tasks");
+  });
+});
