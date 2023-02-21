876175f5dba2c17a5c23fc246d272db12ebbe62f
Felix Odenhall <felix.odenhall@gmail.com>
2023-02-21 12:30:47 +0100

    style select tasks

--- src/components/SelectTasks/SelectTasks.tsx
+++ src/components/SelectTasks/SelectTasks.tsx
@@ -16,23 +16,23 @@ export const SelectTasks = () => {
 
   const itemsList = bingoTasks.map((item) => {
     return (
-      <ListItem key={item.id}>
+      <ListItem key={item.id} textAlign="center">
         <Button
           bg={pickedTasks.includes(item) ? "green" : "white"}
           color={pickedTasks.includes(item) ? "white" : "black"}
           p="5"
-          m="1.5"
+          my="1.5"
           w="95%"
           border="1px"
           borderColor="black"
           borderRadius="2xl"
           fontWeight="semibold"
           fontSize="md"
-          boxShadow="0px 2px 0 0.5px rgba(0, 0, 0, 1)"
+          boxShadow="0 2px 0 0.5px rgba(0, 0, 0, 1)"
           onClick={() => handleClick(item)}
           data-cy="pickTaskButton"
           transition="all .1s ease"
-          _active={{ transform: "translateY(3px)", boxShadow: "none" }}
+          _active={{ transform: "translateY(0.5px)", boxShadow: "none" }}
           _hover={{
             transform: "scale(2px)",
           }}
@@ -48,6 +48,7 @@ export const SelectTasks = () => {
       <UnorderedList
         listStyleType="none"
         mx="auto"
+        mt="4"
         mb="10"
         justifyContent="center"
         px="1"
